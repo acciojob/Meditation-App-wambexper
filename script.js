d2168cc4139c91a8e0503ece136f5c27fbeb9cbf
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-07 16:14:28 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,67 @@
-//your JS code here. If required.
-const video = document.getElementById('video');
-const audio = document.getElementById('audio');
-const timeDisplay = document.querySelector('.time-display');
+const song = document.querySelector(".song");
+const video = document.querySelector(".vid-container video");
+const play = document.querySelector(".play");
+const outline = document.querySelector(".moving-outline circle");
+const outlineLength = outline.getTotalLength();
+let fakeDuration = 600;
+function togglePlay(song) {
+    if (song.paused) {
+        song.play();
+        video.play()
+        play.src = './svg/pause.svg';
 
-let countdown;
-let isPlaying = false;
-
-function playPause() {
-    if (!isPlaying) {
-        audio.play();
-        isPlaying = true;
-        document.querySelector('.play').textContent = 'Pause';
     } else {
-        audio.pause();
-        isPlaying = false;
-        document.querySelector('.play').textContent = 'Play';
+        song.pause();
+        video.pause();
+        play.src = './svg/play.svg';
     }
 }
 
-function changeSound(sound) {
-    audio.src = `sounds/${sound}.mp3`;
-    video.src = `videos/${sound}.mp4`;
-    playPause();
-}
+const app = () => {
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll('.time-select button');
 
-function setTime(seconds) {
-    clearInterval(countdown);
-    let timer = seconds;
-    displayTimeLeft(seconds);
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
 
-    countdown = setInterval(() => {
-        timer--;
-        if (timer < 0) {
-            clearInterval(countdown);
-            isPlaying = false;
-            document.querySelector('.play').textContent = 'Play';
-            return;
-        }
-        displayTimeLeft(timer);
-    }, 1000);
-}
+  play.addEventListener("click", () => {
+      togglePlay(song);
+  });
 
-function displayTimeLeft(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    let remainderSeconds = seconds % 60;
-    if (remainderSeconds < 10) {
-        remainderSeconds = '0' + remainderSeconds;
+  timeSelect.forEach(option => {
+    option.addEventListener("click", function() {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = Math.floor(fakeDuration / 60) + ":" + Math.floor(fakeDuration % 60);
+    });
+  });
+
+  const sounds = document.querySelectorAll(".sound-picker button");
+  sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+      song.src = this.getAttribute("data-sound");
+      video.src = this.getAttribute("data-video");
+      togglePlay(song);
+    });
+  });
+
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    timeDisplay.textContent = minutes + ":" + seconds;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
     }
-    timeDisplay.textContent = `${minutes}:${remainderSeconds}`;
-}
+  };
+};
 
+app();
